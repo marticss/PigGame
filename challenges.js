b3a93f697fae1da42ede05e642c46e3b2d149fd9
marticss <40250297+marticss@users.noreply.github.com>
2019-08-27 09:36:27 +0200

    Add files via upload

--- challenges.js
+++ challenges.js
@@ -1,5 +1,5 @@
 /*
-YOUR 3 CHALLENGES
+
 Change the game to follow these rules:
 
 1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
